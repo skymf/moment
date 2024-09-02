8079979eaff9998c25de6df21b5cb772c878bb94
skymf <divevesha@icloud.com>
2024-09-02 16:15:31 +0200

    hey

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-			},
-			{
-				protocol: "https",
-				hostname: "digitalhippo-production.up.railway.app",
-			},
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "digitalhippo-production.up.railway.app",
+      },
+    ],
+  },
+  experimental: {
+    esmExternals: loose,
+  },
 };
 
 module.exports = nextConfig;
